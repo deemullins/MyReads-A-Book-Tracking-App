5786233a3adc87b4c739dad140fd0686a614b09a
Dee Mullins <mullins.dee@gmail.com>
2018-10-18 16:38:24 -0700

    Update file

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PageNotFound from '../icons/404.jpg';
+
 const NoMatch = () => (
 <div>
 <img src={PageNotFound}  alt="Page Not Found" style={{display: 'block', margin: 'auto', position: 'relative' }} />
 <center><Link to="/">Return to MyReads</Link></center>
 </div>
 );
+
 export default NoMatch;
